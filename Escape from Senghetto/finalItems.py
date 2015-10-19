07c85f2960a25c433dec7e505ccbe80752d5347c
JTCorscadden <jtcorscadden@gmail.com>
2015-10-19 14:06:03 +0100

    Item descriptions added.

--- Escape from Senghetto/finalItems.py
+++ Escape from Senghetto/finalItems.py
@@ -1,18 +1,120 @@
-item_pen = { "id" : "pen" , "name" : "a pen", "damage_beggar": 20, "damage_player": 5}
-item_plastic_knife = { "id" : "plastic knife" , "name" : "a plastic knife", "damage_beggar": 0, "damage_player": 10}
-item_toilet_roll = { "id" : "toilet roll" , "name" : "some toilet roll", "damage_beggar": 0, "damage_player": 10}
-item_frying_pan = { "id" : "frying pan" , "name" : "a frying pan", "damage_beggar": 40, "damage_player": 5}
-item_bulb = { "id" : "bulb" , "name" : "a bulb", "damage_beggar": 30, "damage_player": 30}
-item_sponge = { "id" : "sponge" , "name" : "a sponge", "damage_beggar": 0, "damage_player": 5}
-item_belt = { "id" : "belt" , "name" : "a belt", "damage_beggar": 5, "damage_player": 5}
-item_stone = { "id" : "stone" , "name" : "a stone", "damage_beggar": 30, "damage_player": 5}
-item_pumpkin = { "id" : "pumpkin" , "name" : "a pumpkin", "damage_beggar": 5, "damage_player": 5}
-item_phone = { "id" : "phone" , "name" : "your cell phone", "damage_beggar": 10, "damage_player": 5}
-item_wand = { "id" : "wand" , "name" : "a wand", "damage_beggar": 80, "damage_player": 5}
-item_bacon = { "id" : "bacon" , "name" : "some bacon", "damage_beggar": 0, "damage_player": 5}
-item_laptop = { "id" : "laptop" , "name" : "your laptop", "damage_beggar": 50, "damage_player": 10}
-item_shoes = { "id" : "shoes" , "name" : "beggar's shoes", "damage_beggar": 0, "damage_player": 0}
-item_boxers = { "id" : "boxers" , "name" : "your boxers", "damage_beggar": 0, "damage_player": 0}
-item_top = { "id" : "top" , "name" : "your t-shirt", "damage_beggar": 0, "damage_player": 0}
-item_trousers = { "id" : "trousers" , "name" : "your trousers", "damage_beggar": 0, "damage_player": 0}
+item_pen = 	{"id" : "pen" ,
+			"name": "a pen",
+			"description": "A small green ballpoint pen. Who buys green pens?",
+			"damage_beggar": 20, 
+			"damage_player": 5
+			}
+
+item_plastic_knife = 	{"id" : "plastic knife" , 
+						"name" : "a plastic knife", 
+						"description": "A leftover plastic knife from a late night cheesy chip.",
+						"damage_beggar": 0, 
+						"damage_player": 10
+						}
+
+item_toilet_roll = 	{"id" : "toilet roll" , 
+					"name" : "some toilet roll", 
+					"description": "Some toilet roll. Tesco own brand. It's like sandpaper.",
+					"damage_beggar": 0, 
+					"damage_player": 10
+					}
+
+item_frying_pan = 	{"id" : "frying pan", 
+					"name" : "a frying pan", 
+					"description": "A non-stick frying pan with bits of burnt bacon sticking to it.",
+					"damage_beggar": 40, 
+					"damage_player": 5
+					}
+
+item_bulb = 	{"id" : "bulb" , 
+				"name" : "a bulb", 
+				"description": "It is a bright idea to carry a bulb with you at all times... Bright idea? C'mon guys, tough crowd?",
+				"damage_beggar": 30, 
+				"damage_player": 30
+				}
+
+item_sponge = 	{ "id" : "sponge" , 
+				"name" : "a sponge", 
+				"description": "A sponge. Very absorbant. Doesn't look like it has been used much.",
+				"damage_beggar": 0, 
+				"damage_player": 5
+				}
+
+item_belt = 	{ "id" : "belt" , 
+				"name" : "a belt", 
+				"description": "Standard belt for holding up a pair of trousers. Can also fit around ones head as a hilarious joke when drunk.",
+				"damage_beggar": 5, 
+				"damage_player": 5
+				}
+
+item_stone = 	{ "id" : "stone" , 
+				"name" : "a stone", 
+				"description": "Wow man, that's totally a stone dude.",
+				"damage_beggar": 30, 
+				"damage_player": 5
+				}
+
+item_pumpkin = 	{ "id" : "pumpkin" , 
+				"name" : "a pumpkin",
+				"description": "A pumpkin, there seems to be some sort of phallic symbol carved into the side.", 
+				"damage_beggar": 5, 
+				"damage_player": 5
+				}
+
+item_phone = 	{ "id" : "phone" , 
+				"name" : "your cell phone", 
+				"description": "Your trusty mobile phone. You never go anywhere without it. It's dead, as usual.",
+				"damage_beggar": 10, 
+				"damage_player": 5
+				}
+
+item_wand = 	{ "id" : "wand" , 
+				"name" : "a wand", 
+				"description": "A magic wand, who knows what mysterious items and spells you can conjour with this. Wizard hat sold seperately.",
+				"damage_beggar": 80, 
+				"damage_player": 5
+				}
+
+item_bacon = 	{ "id" : "bacon" , 
+				"name" : "some bacon", 
+				"description": "Delicious bacon, 64'%'' pork, 100'%' amazing. I bet this smells delicious when being cooked.",
+				"damage_beggar": 0, 
+				"damage_player": 5
+				}
+print(item_bacon)
+
+item_laptop = 	{ "id" : "laptop" , 
+				"name" : "your laptop", 
+				"description": "Your laptop is... well it has Microsoft Word and Netflix, and that's good enough for you.",
+				"damage_beggar": 50, 
+				"damage_player": 10
+				}
+
+item_shoes = 	{ "id" : "shoes" , 
+				"name" : "beggar's shoes", 
+				"description": "Apart from the scent of urine, these shoes are quite comfy.",
+				"damage_beggar": 0, 
+				"damage_player": 0
+				}
+
+item_boxers = 	{ "id" : "boxers" , 
+				"name" : "your boxers", 
+				"description": "Calvin Klein boxers. Who are you kidding, you imported them from China for £2.50 a pair.",
+				"damage_beggar": 0, 
+				"damage_player": 0
+				}
+
+item_top = 		{ "id" : "top" , 
+				"name" : "your t-shirt", 
+				"description": "Primarks finest. I hope those yellow stains aren't what they smell like...",
+				"damage_beggar": 0, 
+				"damage_player": 0
+				}
+
+item_trousers = { "id" : "trousers" , 
+				"name" : "your trousers", 
+				"description": "The great thing about jeans is you can wear them even when you rip them. ",
+				"damage_beggar": 0, 
+				"damage_player": 0
+				}
 
